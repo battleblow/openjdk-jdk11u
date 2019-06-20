7f20b7d2b4ea1af609ac3786bec7a168b18ba4b3
Greg Lewis <glewis@eyesbeyond.com>
2019-06-20 18:17:24 +0000

    Implement getting the system processes on NetBSD
    * Use the appropriate sysctls for NetBSD to populate the process list.  The
      code structure is very similar to FreeBSD and should likely be refactored
      once the final shape of the OpenBSD version is clear.
    
    Testing:
    
    * Extracted code and ran in a small standalone program.
    * Compile tested.

--- src/hotspot/os/bsd/os_perf_bsd.cpp
+++ src/hotspot/os/bsd/os_perf_bsd.cpp
@@ -826,8 +826,83 @@ int SystemProcessInterface::SystemProcesses::system_processes(SystemProcess** sy
   *system_processes = next;
 
   return OS_OK;
+#elif defined(__NetBSD__)
+  struct kinfo_proc2 *lproc;
+  int mib[] = { CTL_KERN, KERN_PROC2, KERN_PROC_ALL, 0, sizeof(struct kinfo_proc2), 0 };
+  const u_int miblen = sizeof(mib) / sizeof(mib[0]);
+  size_t length;
+  int pid_count;
+
+  if (sysctl(mib, miblen, NULL, &length, NULL, 0) == -1) {
+    return OS_ERR;
+  }
+
+  lproc = (struct kinfo_proc2 *) malloc(length);
+  if (!lproc) {
+    return OS_ERR;
+  }
+
+  mib[5] = length / sizeof(struct kinfo_proc2);
+
+  if (sysctl(mib, miblen, lproc, &length, NULL, 0) == -1) {
+    free(lproc);
+    return OS_ERR;
+  }
+
+  pid_count = length / sizeof(*lproc);
+  int process_count = 0;
+  SystemProcess *next = NULL;
+
+  for (int i = 0; i < pid_count; i++) {
+    // Executable path
+    int pmib[] = { CTL_KERN, KERN_PROC_ARGS, lproc[i].p_pid, KERN_PROC_PATHNAME };
+    const u_int pmiblen = sizeof(pmib) / sizeof(pmib[0]);
+    char pbuf[PATH_MAX];
+    size_t plen = sizeof(pbuf);
+    if (sysctl(pmib, pmiblen, pbuf, &plen, NULL, 0) == -1) {
+      continue;
+    }
+    plen = strnlen(pbuf, PATH_MAX);
+    if (plen == 0) {
+      continue;
+    }
+    char *path = NEW_C_HEAP_ARRAY(char, plen + 1, mtInternal);
+    strncpy(path, pbuf, plen);
+    path[plen] = '\0';
+
+    // Command line
+    int amib[] = { CTL_KERN, KERN_PROC_ARGS, lproc[i].p_pid, KERN_PROC_ARGV };
+    const u_int amiblen = sizeof(amib) / sizeof(amib[0]);
+    char abuf[ARG_MAX];
+    size_t alen = sizeof(abuf);
+    char *cmdline = NULL;
+    if (sysctl(amib, amiblen, abuf, &alen, NULL, 0) != -1 && alen > 0) {
+      // Arguments are NUL separated in the result, replace that with a space
+      for (size_t j = 0; j < alen; j++) {
+        if (abuf[j] == '\0') {
+          abuf[j] = ' ';
+        }
+      }
+      cmdline = NEW_C_HEAP_ARRAY(char, alen + 1, mtInternal);
+      strncpy(cmdline, abuf, alen);
+      cmdline[alen] = '\0';
+    }
+
+    SystemProcess* current = new SystemProcess();
+    current->set_pid((int)lproc[i].p_pid);
+    current->set_path(path);
+    current->set_command_line(cmdline);
+    current->set_next(next);
+    next = current;
+    process_count++;
+  }
+
+  free(lproc);
+  *no_of_sys_processes = process_count;
+  *system_processes = next;
+
+  return OS_OK;
 #else
-  /* TODO: NetBSD */
   return FUNCTIONALITY_NOT_IMPLEMENTED;
 #endif
 }
