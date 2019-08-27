352e015bcab564bc3b42ae12133b80246f52f2af
MikaelUrankar <mikael.urankar@gmail.com>
2019-08-27 17:56:06 +0200

    Use __clear_cache
    src/hotspot/cpu/arm/icache_arm.cpp:34:27: error: cannot initialize a parameter of type 'char *' with an lvalue of type 'address' (aka 'unsigned char *')
      __builtin___clear_cache(addr, addr + (lines << ICache::log2_line_size));
                              ^~~~
    1 error generated.

--- src/hotspot/cpu/arm/icache_arm.cpp
+++ src/hotspot/cpu/arm/icache_arm.cpp
@@ -67,7 +67,7 @@ static int icache_flush(address addr, int lines, int magic) {
 #else
 
 static int icache_flush(address addr, int lines, int magic) {
-  __builtin___clear_cache(addr, addr + (lines << ICache::log2_line_size));
+  __clear_cache(addr, addr + (lines << ICache::log2_line_size));
   return magic;
 }
 
